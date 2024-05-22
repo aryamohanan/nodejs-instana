c0564453687b35f21a7275f05ab2608574d8dca8
Arya Mohanan <arya.mohanan@ibm.com>
2024-05-22 09:37:29 +0530

    test: test

--- packages/core/src/tracing/instrumentation/frameworks/esm-square-calculator.js
+++ packages/core/src/tracing/instrumentation/frameworks/esm-square-calculator.js
@@ -7,10 +7,9 @@
 
 /* eslint-disable no-unused-vars */
 const shimmer = require('../../shimmer');
-
+const { EXIT } = require('../../constants');
 const requireHook = require('../../../util/requireHook');
 const tracingUtil = require('../../tracingUtil');
-const httpServer = require('../protocols/httpServer');
 const cls = require('../../cls');
 
 let active = false;
@@ -30,14 +29,35 @@ exports.init = function init() {
 
 /**
  * @param {{ calculateSquare: (...args: any[]) => any; }} orgModule
+ * @returns The instrumented module.
  */
 function instrument(orgModule) {
-  //  shimmer.wrap(blabla, 'getSample', shimExpress4Handle);
+  const originalCalculateSquare = orgModule.calculateSquare;
+
   orgModule.calculateSquare = function () {
     const number = arguments[0];
-    console.log(`Calculating the sum of ${number} + ${number}`);
-    return number + number;
+    console.log(`Calculating the square of ${number}`);
+
+    return cls.ns.runAndReturn(() => {
+      const span = cls.startSpan('calculator', EXIT);
+      span.ts = Date.now();
+      span.stack = tracingUtil.getStackTrace(instrument, 1);
+
+      try {
+        span.d = Date.now() - span.ts;
+        span.data.calculator = { number: number, method: 'calculateSquare' };
+        span.transmit();
+        return number + number;
+      } catch (err) {
+        span.ec = 1;
+        span.data.calculator.error = err.message;
+        span.d = Date.now() - span.ts;
+        span.transmit();
+        throw err;
+      }
+    });
   };
+
   return orgModule;
 }
 
